7de1dbd4e1703b9b242eeb84172c295de672eb6b
Miguel Estrada <miguelp.estradam@gmail.com>
2025-04-19 22:31:56 +0100

    feat(controllers): Add GetAll and Create function
    - GetExercises and CreateExcercise Added

--- controllers/exercise_controller.go
+++ controllers/exercise_controller.go
@@ -1,11 +1,69 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+
+	"gym-bro-backend/connection"
 
 	"github.com/gin-gonic/gin"
 )
 
-func GetExercises(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "List"})
+type Exercise struct {
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	Name        string `json:"name" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	PhotoURL    string
+	VideoURL    string
+}
+
+func GetExercises(context *gin.Context) {
+	var exercises []Exercise
+
+	if err := connection.DB.Find(&exercises).Error; err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to retrieve exercises",
+			"error":   err.Error(),
+		})
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message":   "Exercises retrieved successfully",
+		"exercises": exercises,
+	})
+}
+
+func CreateExercise(context *gin.Context) {
+	var newExercise Exercise
+
+	if err := context.ShouldBindJSON(&newExercise); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid request ",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	log.Println("Database connection:", connection.DB)
+
+	// Check if the database connection is initialized
+	if connection.DB == nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Database connection is not initialized",
+		})
+		return
+	}
+
+	if err := connection.DB.Create(&newExercise).Error; err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to create exercise",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Exercise created successfully",
+		"data":    newExercise,
+	})
 }
