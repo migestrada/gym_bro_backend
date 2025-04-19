c6441dac88de0291bfbca2d50e83d10b6fc8003b
Miguel Estrada <miguelp.estradam@gmail.com>
2025-04-19 21:24:22 +0100

    feat(app): initialize server and router registration
    - Added database connection initialization using `Createonnection`
    - Configured Gin router with default middelware
    - Registered application routes by using `RegisterRoutes`
    - Set the server to run on port 8080

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"tgn-backend/connection"
-	"tgn-backend/routes"
+	"gym-bro-backend/connection"
+	"gym-bro-backend/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	connection.CreateConnection()
-	r := gin.Default()
-	routes.RegisterRoutes(r)
-	r.Run()
+	var router *gin.Engine = gin.Default()
+	routes.RegisterRoutes(router)
+	router.Run(":8080")
 }
