c59b24f41759a369d69144e8f73c8f54559ed185
Miguel Estrada <miguelp.estradam@gmail.com>
2025-05-01 14:12:11 +0100

    test(integration): add new test into workout session crud test file
    - update workout session test added

--- tests/integration/workout_session_crud_test.go
+++ tests/integration/workout_session_crud_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
@@ -63,4 +64,28 @@ func TestCreateWorkoutSession(testing *testing.T) {
 	router.ServeHTTP(responseRecorder, req)
 	assert.Equal(testing, http.StatusCreated, responseRecorder.Code)
 	assert.Contains(testing, responseRecorder.Body.String(), "Workout session created successfully")
+}
+
+func TestUpdateWorkoutSession(testing *testing.T) {
+	var router *gin.Engine = setupRouter()
+	router.PUT("/workout_sessions/:id", controllers.UpdateWorkoutSession)
+
+	var workoutSession controllers.WorkoutSession = createTestWorkoutSession()
+	var workoutExercise controllers.WorkoutExercise = createTestWorkoutExercise()
+	var set controllers.Set = createTestSet()
+
+	req, err := http.NewRequest("PUT", "/workout_sessions/"+fmt.Sprint(workoutSession.ID), strings.NewReader(`{
+		"date": "`+fmt.Sprint(time.Now().Format(time.RFC3339))+`",
+		"workout_exercise_id":`+fmt.Sprint(workoutExercise.ID)+`,
+		"set_id":`+fmt.Sprint(set.ID)+`
+	}`))
+
+	if err != nil {
+		testing.Fatal(err)
+	}
+
+	var responseRecorder *httptest.ResponseRecorder = httptest.NewRecorder()
+	router.ServeHTTP(responseRecorder, req)
+	assert.Equal(testing, http.StatusOK, responseRecorder.Code)
+	assert.Contains(testing, responseRecorder.Body.String(), "Workout session updated successfully")
 }
