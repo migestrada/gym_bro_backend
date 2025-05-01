5fdb16978a3d8ae8191292812f2bcb586cadeabb
Miguel Estrada <miguelp.estradam@gmail.com>
2025-05-01 11:28:22 +0100

    test(integration) add update workout exercise into crud tests

--- tests/integration/workout_exercise_crud_test.go
+++ tests/integration/workout_exercise_crud_test.go
@@ -57,3 +57,20 @@ func TestCreateWorkoutExercise(test *testing.T) {
 	assert.Equal(test, http.StatusCreated, responseRecorder.Code)
 	assert.Contains(test, responseRecorder.Body.String(), "Workout exercise created successfully")
 }
+
+func TestUpdateWorkoutExercise(test *testing.T) {
+	var router *gin.Engine = setupRouter()
+	router.PUT("/workout_exercises/:id", controllers.UpdateWorkoutExercise)
+
+	var workoutExercise controllers.WorkoutExercise = createTestWorkoutExercise()
+	var workout controllers.Workout = createTestWorkout()
+	var exercise controllers.Exercise = createTestExercise()
+	req, err := http.NewRequest("PUT", "/workout_exercises/"+fmt.Sprint(workoutExercise.ID), strings.NewReader(`{"workout_id":`+fmt.Sprint(workout.ID)+`,"exercise_id":`+fmt.Sprint(exercise.ID)+`}`))
+	if err != nil {
+		test.Fatal(err)
+	}
+	var responseRecorder *httptest.ResponseRecorder = httptest.NewRecorder()
+	router.ServeHTTP(responseRecorder, req)
+	assert.Equal(test, http.StatusOK, responseRecorder.Code)
+	assert.Contains(test, responseRecorder.Body.String(), "Workout exercise updated successfully")
+}
