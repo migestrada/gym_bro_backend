c37fdf68b78596387468c0f925e3077d70de9b66
Miguel Estrada <miguelp.estradam@gmail.com>
2025-05-01 11:26:32 +0100

    refactor(model) add workout exercises association into exercise and workout

--- models/exercise.go
+++ models/exercise.go
@@ -1,13 +1,14 @@
 package models
 
 type Exercise struct {
-	ID              uint   `gorm:"primaryKey"`
-	Name            string `grom:"unique;not null"`
-	Description     string
-	PhotoURL        string `json:"photo_url"`
-	VideoURL        string `json:"video_url"`
-	CreatedAt       int64
-	UpdatedAt       int64
-	WorkoutSessions []WorkoutSession `gorm:"foreignKey:ExerciseID"`
-	Sets            []Set            `gorm:"foreignKey:ExerciseID"`
+	ID               uint   `gorm:"primaryKey"`
+	Name             string `grom:"unique;not null"`
+	Description      string
+	PhotoURL         string `json:"photo_url"`
+	VideoURL         string `json:"video_url"`
+	CreatedAt        int64
+	UpdatedAt        int64
+	WorkoutSessions  []WorkoutSession  `gorm:"foreignKey:ExerciseID"`
+	Sets             []Set             `gorm:"foreignKey:ExerciseID"`
+	WorkoutExercises []WorkoutExercise `gorm:"foreignKey:ExerciseID"`
 }
--- models/workout.go
+++ models/workout.go
@@ -3,12 +3,13 @@ package models
 import "time"
 
 type Workout struct {
-	ID              uint             `gorm:"primaryKey"`
-	Order           int              `gorm:"not null;uniqueIndex:idx_training_plan_order,composite:order_plan"`
-	Name            string           `gorm:"not null"`
-	WorkoutSessions []WorkoutSession `gorm:"foreignKey:WorkoutID"`
-	TrainingPlanID  uint             `gorm:"not null;uniqueIndex:idx_training_plan_order,composite:order_plan"`
-	TrainingPlan    TrainingPlan     `gorm:"foreignKey:TrainingPlanID"`
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
+	ID               uint              `gorm:"primaryKey"`
+	Order            int               `gorm:"not null;uniqueIndex:idx_training_plan_order,composite:order_plan"`
+	Name             string            `gorm:"not null"`
+	WorkoutSessions  []WorkoutSession  `gorm:"foreignKey:WorkoutID"`
+	TrainingPlanID   uint              `gorm:"not null;uniqueIndex:idx_training_plan_order,composite:order_plan"`
+	TrainingPlan     TrainingPlan      `gorm:"foreignKey:TrainingPlanID"`
+	WorkoutExercises []WorkoutExercise `gorm:"foreignKey:WorkoutID"`
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
 }
